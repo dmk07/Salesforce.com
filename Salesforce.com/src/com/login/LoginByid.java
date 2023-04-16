4c94aca7a6615c554cfd2bf2e6c10bade456034e
dmk61 <dmk61@Dipak-Kadam>
2023-04-16 10:56:07 +0530

    login functionality tested by id & name locators

--- Salesforce.com/src/com/login/LoginByid.java
+++ Salesforce.com/src/com/login/LoginByid.java
@@ -1,5 +1,6 @@
 package com.login;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -14,14 +15,32 @@ public class LoginByid {
 				
 		WebDriver driver=new EdgeDriver();
 	
+	// launch the browser with url
 	public void LaunchAUT()
 	{
-		
+		driver.manage().window().maximize();
+		driver.navigate().to("https://login.salesforce.com/?locale=in");
+		// driver.get("uRL");
+		driver.manage().window().maximize();
 	}
+	
+	//test login functionality
+	
+	public void testLogin()
+	{
+		driver.findElement(By.id("username")).sendKeys("dipak100kadam@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("dklajf");
+		driver.findElement(By.id("Login")).click();
+	}
+	
 	
 	
 	public static void main(String[] args) {
 	
+		
+		LoginByid test=new LoginByid();
+		test.LaunchAUT();
+		test.testLogin();
 		
 		
 		
---
+++ Salesforce.com/src/com/login/LoginByname.java
@@ -0,0 +1,50 @@
+package com.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LoginByname {
+	
+	static {
+		//Dipak
+				System.setProperty("webdriver.edge.driver", "C://msedgedriver.exe");
+				// dont create obj in static block you cant acess in local blcok
+
+			}
+			
+	WebDriver driver=new EdgeDriver();
+
+// launch the browser with url
+public void LaunchAUT()
+{
+	driver.manage().window().maximize();
+	driver.navigate().to("https://login.salesforce.com/?locale=in");
+	// driver.get("uRL");
+	driver.manage().window().maximize();
+}
+
+//test login functionality
+
+public void testLogin()
+{
+	driver.findElement(By.name("username")).sendKeys("dipak100kadam@gmail.com");
+	driver.findElement(By.name("pw")).sendKeys("dklajf");
+	driver.findElement(By.name("Login")).click();
+}
+
+
+
+public static void main(String[] args) {
+
+	
+	LoginByid test=new LoginByid();
+	test.LaunchAUT();
+	test.testLogin();
+	
+	
+	
+}
+	
+
+}
