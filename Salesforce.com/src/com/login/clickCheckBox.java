654747d308ece4edb07d5a5c6fb9e9a09d68a620
dmk61 <dmk61@Dipak-Kadam>
2023-04-18 23:12:17 +0530

    checkbox by id and name

---
+++ Salesforce.com/src/com/login/clickCheckBox.java
@@ -0,0 +1,47 @@
+package com.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class clickCheckBox {
+	static {
+		//Dipak
+				System.setProperty("webdriver.edge.driver", "C://msedgedriver.exe");
+				// dont create obj in static block you cant acess in local blcok
+			}
+	
+	
+	WebDriver driver=new EdgeDriver();
+	
+	public void LaunchAUT()
+	{
+		driver.manage().window().maximize();
+		driver.navigate().to("https://login.salesforce.com/?locale=in");
+		// driver.get("uRL");
+		driver.manage().window().maximize();
+	}
+	
+	public void clickCheckBoxByid() throws InterruptedException
+	{
+		driver.findElement(By.id("rememberUn")).click();
+		Thread.sleep(2000);
+	}
+	
+	public void clickCheckBoxByname() throws InterruptedException
+	{Thread.sleep(2000);
+		driver.findElement(By.id("rememberUn")).click();
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+
+		clickCheckBox click=new clickCheckBox();
+		click.LaunchAUT();
+		click.clickCheckBoxByid();
+		
+		click.clickCheckBoxByname();
+		
+		
+	}
+
+}
