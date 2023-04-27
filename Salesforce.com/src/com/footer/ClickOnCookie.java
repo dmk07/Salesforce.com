7c0dd1bb2b967703341b321039867b9a09acf15d
dmk61 <dmk61@Dipak-Kadam>
2023-04-27 15:11:57 +0530

    salesforce testing completed

--- Salesforce.com/src/com/footer/ClickOnCookie.java
+++ Salesforce.com/src/com/footer/ClickOnCookie.java
@@ -1,5 +1,43 @@
 package com.footer;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class ClickOnCookie {
+	
+	
+	static {
+		//Dipak
+				System.setProperty("webdriver.edge.driver", "C://msedgedriver.exe");
+				// dont create obj in static block you cant acess in local blcok
+			}
+	
+	
+	WebDriver driver=new EdgeDriver();
+	
+	public void LaunchAUT()
+	{
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.salesforce.com/in/form/signup/freetrial-sales/?d=jumbo1-btn-ft");
+		// driver.get("uRL");
+		driver.manage().window().maximize();
+	}
+	
+	public void clickonCookie()
+	{
+		driver.findElement(By.xpath("/html/body/footer/div/div[1]/div/div/div/div/div[2]/nav/ul/li[7]/a")).click();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		ClickOnCookie click=new ClickOnCookie();
+		click.LaunchAUT();
+		click.clickonCookie();
+		
+	}
+
 
 }
--- Salesforce.com/src/com/footer/ClickOnYourPrivacy.java
+++ Salesforce.com/src/com/footer/ClickOnYourPrivacy.java
@@ -1,5 +1,42 @@
 package com.footer;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class ClickOnYourPrivacy {
+	
+	
+	static {
+		//Dipak
+				System.setProperty("webdriver.edge.driver", "C://msedgedriver.exe");
+				// dont create obj in static block you cant acess in local blcok
+			}
+	
+	
+	WebDriver driver=new EdgeDriver();
+	
+	public void LaunchAUT()
+	{
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.salesforce.com/in/form/signup/freetrial-sales/?d=jumbo1-btn-ft");
+		// driver.get("uRL");
+		driver.manage().window().maximize();
+	}
+	
+	public void clickonPrivacy()
+	{
+		driver.findElement(By.xpath("/html/body/footer/div/div[1]/div/div/div/div/div[2]/nav/ul/li[8]/a")).click();
+	}
+	
+	public static void main(String args[])
+	{
+		ClickOnPrivacy obj=new ClickOnPrivacy();
+		
+		obj.LaunchAUT();
+		obj.clickonPrivacy();
+	}
+	
+
 
 }
